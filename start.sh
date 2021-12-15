72bcd29caec4754ef4ca5d398c5db4b82a3d53bd
- Evan ShaKuR <96147328+xrzo@users.noreply.github.com>
2021-12-15 03:29:24 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/RR9R7/Youtube.git /AnyDLBot
+git clone https://github.com/xrzo/Youtube.git /AnyDLBot
 cd /Youtube
 pip3 install -r requirements.txt
 echo "Starting Bot...."
